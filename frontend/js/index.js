97d57f3fd369c05a4c0bb03f37a49f20b5004f2d
madhumitha <24wh1a05b4@bvrithyderabad.edu.in>
2026-06-20 10:01:24 +0530

    updated toggle

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -3,28 +3,21 @@ const maleUser = {
     gender: "Male",
     image: "../images/john.png"
 };
-
 const femaleUser = {
     name: "Jane Doe",
     gender: "Female",
     image: "../images/jane.png"
 };
-
 let currentUser = maleUser;
-
 function toggleUser() {
-
     currentUser =
         currentUser.gender === "Male"
             ? femaleUser
             : maleUser;
-
     document.getElementById("userName").innerText =
         currentUser.name;
-
     document.getElementById("userGender").innerText =
         currentUser.gender;
-
     document.getElementById("userImage").src =
         currentUser.image;
 }
